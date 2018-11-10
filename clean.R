bea449d5f45da2c4d0d8e5698718e6f4f2f60e56
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-10 17:38:18 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,4 +1,5 @@
 
+#Cleaning Satisfaction Column
 change <- c(which(grepl("4.00.2.00",rawData$Satisfaction)),which(grepl("4.00.5",rawData$Satisfaction)))
 change
 rawData$Satisfaction <- replace(rawData$Satisfaction,list = change,4)
