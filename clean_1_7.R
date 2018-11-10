c6a9de996402748426537b6599604e41599e2214
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-10 17:18:31 -0500

    Saving clean_1_7.R

--- clean_1_7.R
+++ clean_1_7.R
@@ -11,5 +11,5 @@ library(tidyverse)
 ##############
 change <- c(which(grepl("4.00.2.00",rawData$Satisfaction)),which(grepl("4.00.5",rawData$Satisfaction)))
 change
-rawData$atisfaction <- replace(rawData$Satisfaction,list = change,4)
+rawData$Satisfaction <- replace(rawData$Satisfaction,list = change,4)
 
