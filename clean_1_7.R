83300381eee604a639e4bee28882975649424550
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-10 17:19:21 -0500

    Saving clean_1_7.R

--- clean_1_7.R
+++ clean_1_7.R
@@ -10,4 +10,4 @@
 change <- c(which(grepl("4.00.2.00",rawData$Satisfaction)),which(grepl("4.00.5",rawData$Satisfaction)))
 change
 rawData$Satisfaction <- replace(rawData$Satisfaction,list = change,4)
-rawData$Satisfaction
+Summary(rawData$Satisfaction)
