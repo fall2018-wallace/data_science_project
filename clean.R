37c17436081d5e8d9a36ce25cdf13acbed1f17a1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-10 14:23:43 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,4 +1,4 @@
 
-rawData <- read.csv("df")
+rawData <- df
 colnames(rawData) <- c("Satisfaction", "Airline_status","Age","Gender","Price_sensitivity", "Year_of_flights","No_of_flights_pa","Percent_of_flights_with_other_airlines","Type_of_travel","No_of_other_loyalty_cards","Shopping_amount_at_airport","Eating_and_drinking_at_airport","Class","Day_of_month","Flight_date","Airline_code","Airline_name","Origin_city","Origin_state","Destination_city","Destination_state","Scheduled_departure_hour","Departure_delay_in_minutes","Arrival_delay_in_minutes","Flight_cancelled","Flight_time_in_minutes","Flight_distance","Arrival_delay_greater_than_5minutes")
 rawData
