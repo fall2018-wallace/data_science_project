412cc7cb261432b8e687e8986a1738806b0f520e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-15 13:35:56 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -8,3 +8,12 @@ rawData$Satisfaction <- replace(rawData$Satisfaction,list = change,4)
 table(rawData$Satisfaction)
 
 #Cleaning Flight Date Column
+
+
+
+rawData$Departure_delay_in_minutes[is.na(rawData$Departure_delay_in_minutes)] <- 0
+sum(is.na(rawData$Departure_delay_in_minutes))
+
+summary(rawData$Departure_delay_in_minutes)
+
+str(rawData$Departure_delay_in_minutes)
