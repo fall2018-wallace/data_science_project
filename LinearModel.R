0073e2cc8e8b4c1ef69a7ec9d86b02b0d554c962
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-17 19:39:33 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -5,7 +5,7 @@ summary(mode1) #0.1184
 mode2 <- lm(Satisfaction ~ Age,data = cleanData)
 summary(mode2) #0.0492
 
-mode3 <- lm(Satisfaction ~ Gender,data = rawData)
+mode3 <- lm(Satisfaction ~ Gender,data = cleanData)
 summary(mode3) #0.0492
 
 
