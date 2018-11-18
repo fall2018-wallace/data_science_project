7ca7d2f541cefed3949c5128da3fc831975e3cbe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-17 19:31:54 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -1,9 +1,9 @@
 
-linearmodel <- lm(Satisfaction ~ Airline_status,data = rawData)
-summary(linearmodel) #0.1184
+mode1 <- lm(Satisfaction ~ Airline_status,data = rawData)
+summary(mode1) #0.1184
 
-linearmodel <- lm(Satisfaction ~ Day_of_month,data = rawData)
-summary(linearmodel) #0.0492
+mode2 <- lm(Satisfaction ~ Day_of_month,data = rawData)
+summary(mode2) #0.0492
 
 
 table(rawData$Satisfaction)
