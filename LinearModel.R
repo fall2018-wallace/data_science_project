de52aec70d3cc21782dd7b300156fadbfe81d27b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-17 19:31:18 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -1,3 +1,22 @@
 
 linearmodel <- lm(Satisfaction ~ Airline_status,data = rawData)
 summary(linearmodel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+m1<- lm(formula = Satisfaction ~ Airline_status + Type_of_travel,rawData = data)
+summary(m1) #r=0.3773
