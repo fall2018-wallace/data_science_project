a6a2c679dcf41dd05b53faa8a2643f37b8d7859a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-17 19:59:07 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -43,3 +43,6 @@ summary(mod14) #0.001426
 
 mod15 <- lm(Satisfaction ~ Airline_code,data = cleanData)
 summary(mod15) #0.0006697
+
+mod16 <- lm(Satisfaction ~ Airline_name,data = cleanData)
+summary(mod15)
