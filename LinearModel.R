b562b1415dd228280151f9963485a639bbb331cc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-17 19:44:19 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -1,15 +1,15 @@
 
 mod1 <- lm(Satisfaction ~ Airline_status,data = cleanData)
-summary(mode1) #0.1184
+summary(mod1) #0.1184
 
 mod2 <- lm(Satisfaction ~ Age,data = cleanData)
-summary(mode2) #0.0492
+summary(mod2) #0.0492
 
 mod3 <- lm(Satisfaction ~ Gender,data = cleanData)
-summary(mode3) #0.01761
+summary(mod3) #0.01761
 
 mod4 <- lm(Satisfaction ~ Price_sensitivity,data = cleanData)
-summary(mode4) #0.007637
+summary(mod4) #0.007637
 
 mod5 <- lm(Satisfaction ~ Year_of_flights,data = cleanData)
 summary(mod5)
