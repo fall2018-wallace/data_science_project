9cf42c7a525f2fd41badc0c7d02ebaf35cc40425
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-17 19:30:09 -0500

    force updated content when merge failed

--- clean.R
+++ clean.R
@@ -1,6 +1,19 @@
 
+library(dplyr)
+
 #Cleaning Satisfaction Column
 change <- c(which(grepl("4.00.2.00",rawData$Satisfaction)),which(grepl("4.00.5",rawData$Satisfaction)))
 change
 rawData$Satisfaction <- replace(rawData$Satisfaction,list = change,4)
 table(rawData$Satisfaction)
+
+#Cleaning Flight Date Column
+
+
+
+rawData$Departure_delay_in_minutes[is.na(rawData$Departure_delay_in_minutes)] <- 0
+sum(is.na(rawData$Departure_delay_in_minutes))
+
+summary(rawData$Departure_delay_in_minutes)
+
+str(rawData$Departure_delay_in_minutes)
