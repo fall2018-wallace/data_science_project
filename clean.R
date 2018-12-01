e0d5b99f43a0d5a97fe5c36e640e39a13903b6a1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-30 21:50:24 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,6 +1,4 @@
 
-library(dplyr)
-
 #Cleaning Satisfaction Column
 change <- c(which(grepl("4.00.2.00",rawData$Satisfaction)),which(grepl("4.00.5",rawData$Satisfaction)))
 change
@@ -14,4 +12,10 @@ wasteData
 cleanData <- rawData[which(rawData$Flight_cancelled=='No'),]
 cleanData
 
-cleanData <- rawData
+#Replacing NA's in Arrival_delay_in_minutes by 0's
+index <- which(is.na(cleanData$Arrival_delay_in_minutes))
+cleanData$Arrival_delay_in_minutes <- replace(cleanData$Arrival_delay_in_minutes, is.na(cleanData$Arrival_delay_in_minutes),0)
+
+#Replacing NA's in Flight_time_in_minutes by mean flight time
+mean_time <- mean(na.omit(cleanData$Flight_time_in_minutes))
+cleanData$Flight_time_in_minutes <- replace(cleanData$Flight_time_in_minutes, is.na(cleanData$Flight_time_in_minutes),mean_time)
