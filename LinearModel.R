ac069b1ba429f512e21d3228c05df61630cfa1c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-30 22:22:02 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -81,11 +81,11 @@ mod27 <- lm(Satisfaction ~ Arrival_delay_greater_than_5minutes,data = cleanData)
 summary(mod27) #0.02529 
 
 linearmodel <- lm(Satisfaction ~ Type_of_travel + Airline_status + Arrival_delay_greater_than_5minutes,data = cleanData)
-summary(linearmodel)#r=0.435
+summary(linearmodel)##r=0.435
 
 
 m1<- lm(formula = Satisfaction ~ Airline_status + Type_of_travel ,cleanData)
-summary(m1)#0.4084
+summary(m1)##0.4084
 m2<- lm(formula = Satisfaction ~ Type_of_travel + Class,cleanData)
 summary(m2) #r=0.3358
 
@@ -111,7 +111,7 @@ m9 <- lm(formula = Satisfaction ~ Arrival_delay_greater_than_5minutes + Type_of_
 summary(m9) #r= 0.3619
 
 m10 <- lm(formula = Satisfaction ~ Arrival_delay_greater_than_5minutes+Type_of_travel + Airline_status ,cleanData)
-summary(m10) #r= 0.4349
+summary(m10) ##r= 0.4349
 
 m11 <- lm(formula = Satisfaction ~ Departure_delay_in_minutes +Type_of_travel + Gender + Airline_status + Origin_state ,cleanData)
-summary(m11) #0.4179
+summary(m11) ##r0.4179
