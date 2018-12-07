e97fddbe272a6968561a0ccd532ce478e6e4ec85
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 14:32:58 -0500

    Saving Apriori_resultplots.R

--- Apriori_resultplots.R
+++ Apriori_resultplots.R
@@ -6,3 +6,5 @@ freq_all <- itemFrequencyPlot(df,support=0.1)
 
 lift_sub <- hist(quality(rules)$lift)
 rules_sub <- plot(rules,jitter = 0)
+goodrules_sub <- plot(goodrules)
+freq_sub <- itemFrequencyPlot(df,support=0.1)
