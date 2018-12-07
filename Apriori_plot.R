2c9c0bfa80a71da6c20e0a1c54fb49487dbb4ea6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 13:53:42 -0500

    Saving Apriori_plot.R

--- Apriori_plot.R
+++ Apriori_plot.R
@@ -1,13 +1,14 @@
 
-sat_all <- hist(cleanData$Satisfaction, main = "Histogram for Satisfaction of all airlines", xlab = "Satisfaction")
+sat <- hist(cleanData$Satisfaction, main = "Histogram for Satisfaction of all airlines", xlab = "Satisfaction")
 
-ps_all <- hist(cleanData$Price_sensitivity, main= "Histogram for price sensitivity of all airlines",xlab = "Price Sensitivity")
+ps <- hist(cleanData$Price_sensitivity, main= "Histogram for price sensitivity of all airlines",xlab = "Price Sensitivity")
 
-year_all <- boxplot(cleanData$Year_of_flights)
+year <- boxplot(cleanData$Year_of_flights)
 
-no_all <- hist(cleanData$No_of_flights_pa) 
+no <- hist(cleanData$No_of_flights_pa) 
 
-per_all <- hist(cleanData$Percent_of_flights_with_other_airlines)
+per <- hist(cleanData$Percent_of_flights_with_other_airlines)
 
-date_all <- table(cleanData$Flight_date)
+date <- table(cleanData$Flight_date)
 
+dep <- table(cleanData$Departure_delay_in_minutes)
