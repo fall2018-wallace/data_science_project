013e3dc9b1bc48ad4fafe01ab956deb618a1d62a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 18:47:43 -0500

    Saving renaming.R

--- renaming.R
+++ renaming.R
@@ -1,5 +1,4 @@
 
-data=read.csv("C:\\Users\\Harsh Darji\\Desktop\\SS.csv", header=TRUE)
 colnames(data)<-c("satisfaction","airline_status","age","gender","price_sensitivity","year_of_first_flight","no_of_flights_per_annum",
                   "percent_of_flights_with_other_airlines","type_of_travel","no_of_loyalty_cards","shopping_amount_at_airport",
                   "eat_drink_at_airport","class","day_of_month","flight_date","airline_code","airline_name","origin_city",
