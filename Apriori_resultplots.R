5d9046d0cc8105a654f50ce890254a19cfd335bc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 02:11:28 -0500

    force updated content when merge failed

--- Apriori_resultplots.R
+++ Apriori_resultplots.R
@@ -1,3 +1,13 @@
 
+library(arulesViz)
+library(arules)
+
 lift_all <- hist(quality(rules)$lift)
 rules_all <- plot(rules,jitter = 0)
+goodrules_all <- plot(goodrules)
+freq_all <- itemFrequencyPlot(df,support=0.1)
+
+lift_sub <- hist(quality(rules1)$lift)
+rules_sub <- plot(rules1,jitter = 0)
+goodrules_sub <- plot(goodrules1)
+freq_sub <- itemFrequencyPlot(df1,support=0.1)
