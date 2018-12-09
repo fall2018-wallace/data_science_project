6225c7d55f7b41b2983dbd4e3a2b2041af7a8ea2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 18:50:34 -0500

    Saving renaming.R

--- renaming.R
+++ renaming.R
@@ -1,5 +1,5 @@
 
-colnames(df)<-c("satisfaction","airline_status","age","gender","price_sensitivity","year_of_first_flight","no_of_flights_per_annum",
+colnames(rawdata)<-c("satisfaction","airline_status","age","gender","price_sensitivity","year_of_first_flight","no_of_flights_per_annum",
                   "percent_of_flights_with_other_airlines","type_of_travel","no_of_loyalty_cards","shopping_amount_at_airport",
                   "eat_drink_at_airport","class","day_of_month","flight_date","airline_code","airline_name","origin_city",
                   "origin_state","destination_city","destination_state","schedule_departure_hour","departure_delay_in_minutes",
