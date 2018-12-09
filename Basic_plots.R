3617f2a8e4e23b9e26f64002c5c2c64dca453b85
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-08 19:40:23 -0500

    Saving Basic_plots.R

--- Basic_plots.R
+++ Basic_plots.R
@@ -43,3 +43,38 @@ g2 <- ggplot(sat_type, aes(x=Type.of.Travel, y=Satisfaction)) +
        subtitle="Type of travel Vs Satisfaction") +  
   coord_flip()
 
+
+# Plot for Arrival delay greater than 5 mins
+sat_delay <- aggregate(cleanData$Satisfaction, by=list(cleanData$Arrival_delay_greater_than_5minutes), FUN=mean)  # aggregate
+colnames(sat_delay) <- c("Arrival.delay.greater.than.5.min", "Satisfaction")  # change column names
+sat_delay <-sat_delay[order(sat_delay$Satisfaction), ]  # sort
+sat_delay$Arrival.delay.greater.than.5.min <- factor(sat_delay$Arrival.delay.greater.than.5.min, levels = sat_delay$Arrival.delay.greater.than.5.min)  # to retain the order in plot.
+head(sat_delay, 4)
+#plot
+g3 <- ggplot(sat_delay, aes(x=Arrival.delay.greater.than.5.min, y=Satisfaction)) + 
+  geom_point(size=3, color="Red") + 
+  geom_segment(aes(x=Arrival.delay.greater.than.5.min, 
+                   xend=Arrival.delay.greater.than.5.min, 
+                   y=0,  yend=Satisfaction)) + 
+  labs(title="Lollipop Chart", 
+       subtitle="Arrival delay greater than 5 mins  VS  Avg.Satisfaction")
+
+
+# Histogram on a Gender variable
+g4 <- ggplot(cleanData, aes(Gender))
+g4 + geom_bar(aes(fill=Airline_status), width = 0.5) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Frequency for Gender", 
+       subtitle="Gender across Airline Status") + ylab()
+
+q <- quantile(cleanData$Age, c(0.2, 0.75)) #age is cut into quantiles
+cleanData$Age_category <- replicate(length(cleanData$Age), "Middle Age")
+cleanData$Age_category[cleanData$Age <= q[1]] <- "Young" #if age falls within 1st quartile vector's field  is set as low
+cleanData$Age_category[cleanData$Age > q[2]] <- "Old" ##if age falls within 2nd quartile vector's field  is set as high
+
+# Histogram on a Gender variable
+g5 <- ggplot(cleanData, aes(Age_category))
+g5 + geom_bar(aes(fill=satisfied), width = 0.5) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Frequency for Age Groups", 
+       subtitle="Age Groups across Satisfied") 
