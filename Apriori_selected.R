96e8bd755cbd637bfd2505add0fed2c255f1ed4b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:09:31 -0500

    Saving Apriori_selected.R

--- Apriori_selected.R
+++ Apriori_selected.R
@@ -9,10 +9,6 @@ satisfied <- ifelse(cleanData$Satisfaction < 3.5,"no","yes")
 pricesensitivity <- ifelse(cleanData$Price_sensitivity<3,"low","high")
 Flightspa <- ifelse(cleanData$No_of_flights_pa <40, "low", "high")
 percentflightwithotherAirlines <- ifelse(cleanData$Percent_of_flights_with_other_airlines<=10,"less than 10","more then 10")
-Month <- replicate(length(cleanData$Flight_date),"January")
-Month[month(mdy(cleanData$Flight_date))==2] <- "February"
-Month[month(mdy(cleanData$Flight_date))==3] <- "March"
-head(Month)
 
 Departuredelay <- replicate(length(cleanData$Departure_delay_in_minutes),"Average")
 Departuredelay[cleanData$Departure_delay_in_minutes<=60] <- "low"
