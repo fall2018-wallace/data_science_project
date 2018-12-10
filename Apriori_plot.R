ae458fff339ddf9b97bd2b2c22778a14e11f54ae
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:22:16 -0500

    Saving Apriori_plot.R

--- Apriori_plot.R
+++ Apriori_plot.R
@@ -1,3 +1,6 @@
 
-hist(quality(rules)$lift)
-plot(rules,jitter = 0)
+Histogram_rules <- hist(quality(rules)$lift)
+plot_rules <- plot(rules,jitter = 0)
+
+plot_goodrules <- plot(goodrules)
+Itemfrequencyplot <- itemFrequencyPlot(df,support=0.1)
