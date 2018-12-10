c7abdb803abf1ca7296d7788af3dc44b0b996702
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:19:21 -0500

    Saving Apriori_selected.R

--- Apriori_selected.R
+++ Apriori_selected.R
@@ -18,4 +18,9 @@ Age[cleanData$Age > q[2]] <- "High"
 
 
 df1 <- data.frame(satisfied, cleanData$Airline_status, cleanData$Type_of_travel,  Flightspa, Age, cleanData$Arrival_delay_greater_than_5minutes)
+df1 <- as(df1, "transactions")              
+itemFrequency(df1)
 
+rules1<-apriori(df1,parameter = list(support=0.1, confidence=0.5),appearance = list(default="lhs", rhs=("satisfied=no")))
+summary(rules1)
+inspect(rules1)
