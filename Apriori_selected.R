f94bcff22aefe08ac9364da37830ec56c7dc7a9a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:16:35 -0500

    Saving Apriori_selected.R

--- Apriori_selected.R
+++ Apriori_selected.R
@@ -5,6 +5,7 @@ library(arules)
 library(arulesViz)
 library(dplyr)
 cleanData$Satisfaction <- as.numeric(as.character(cleanData$Satisfaction))
+
 satisfied <- ifelse(cleanData$Satisfaction < 3.5,"no","yes")
 
 Flightspa <- ifelse(cleanData$No_of_flights_pa <40, "low", "high")
@@ -13,6 +14,8 @@ q <- quantile(cleanData$Age, c(0.4, 0.6))
 Age <- replicate(length(cleanData$Age), "Average")
 Age[cleanData$Age <= q[1]] <- "Low"
 Age[cleanData$Age > q[2]] <- "High"
+
+
 
 df1 <- data.frame(satisfied, cleanData$Airline_status, cleanData$Type_of_travel,  Flightspa, Age, cleanData$Arrival_delay_greater_than_5minutes)
 
