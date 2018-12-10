9c4ac5f445c6ef2585607a7abba5bfda529fe259
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:01:15 -0500

    Saving Apriori_selected.R

--- Apriori_selected.R
+++ Apriori_selected.R
@@ -1,3 +1,4 @@
 
 cleanData$Satisfaction <- as.numeric(as.character(cleanData$Satisfaction))
 satisfied <- ifelse(cleanData$Satisfaction < 3.5,"no","yes")
+pricesensitivity <- ifelse(cleanData$Price_sensitivity<3,"low","high")
