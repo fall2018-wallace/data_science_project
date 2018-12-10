44a413ae5d640a546678e12ca61a18922c89130a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:01:40 -0500

    Saving Apriori_selected.R

--- Apriori_selected.R
+++ Apriori_selected.R
@@ -2,3 +2,4 @@
 cleanData$Satisfaction <- as.numeric(as.character(cleanData$Satisfaction))
 satisfied <- ifelse(cleanData$Satisfaction < 3.5,"no","yes")
 pricesensitivity <- ifelse(cleanData$Price_sensitivity<3,"low","high")
+Flightspa <- ifelse(cleanData$No_of_flights_pa <40, "low", "high")
