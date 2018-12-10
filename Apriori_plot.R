22527dcbc86258c1d61e8518bc505a4b3b1c71c6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 20:06:25 -0500

    Saving Apriori_plot.R

--- Apriori_plot.R
+++ Apriori_plot.R
@@ -3,7 +3,7 @@ library(arulesViz)
 library(arules)
 
 Histogram_rules <- hist(lifts)
-plot_rules <- plot(rules)
+plot_rules <- plot(rules, jitter=0)
 
 plot_goodrules <- plot(goodrules)
 Itemfrequencyplot <- itemFrequencyPlot(df,support=0.1)
