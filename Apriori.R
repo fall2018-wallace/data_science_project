466cdc25862dd13f6a09d3f5041ecb67a741625b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 20:35:03 -0500

    Saving Apriori.R

--- Apriori.R
+++ Apriori.R
@@ -6,48 +6,34 @@ library(arulesViz)
 
 cleanData$Satisfaction <- as.numeric(as.character(cleanData$Satisfaction))
 satisfied <- ifelse(cleanData$Satisfaction < 3.5,"no","yes")
+
 pricesensitivity <- ifelse(cleanData$Price_sensitivity<3,"low","high")
 
-boxplot(cleanData$Year_of_flights)
-
-hist(cleanData$No_of_flights_pa)
 Flightspa <- ifelse(cleanData$No_of_flights_pa <40, "low", "high")
 
-hist(cleanData$Percent_of_flights_with_other_airlines)
 percentflightwithotherAirlines <- ifelse(cleanData$Percent_of_flights_with_other_airlines<=10,"less than 10","more then 10")
-
-table(cleanData$Flight_date)
 
 Month <- replicate(length(cleanData$Flight_date),"January")
 Month[month(mdy(cleanData$Flight_date))==2] <- "February"
 Month[month(mdy(cleanData$Flight_date))==3] <- "March"
 head(Month)
 
-table(cleanData$Departure_delay_in_minutes)
 Departuredelay <- replicate(length(cleanData$Departure_delay_in_minutes),"Average")
 Departuredelay[cleanData$Departure_delay_in_minutes<=60] <- "low"
-
 Departuredelay[cleanData$Departure_delay_in_minutes>180] <- "High"
 
-
-table(cleanData$Arrival_delay_in_minutes)
 Arrivaldelay <- replicate(length(cleanData$Arrival_delay_in_minutes),"Average")
 Arrivaldelay[cleanData$Arrival_delay_in_minutes<=60] <- "low"
-
 Arrivaldelay[cleanData$Arrival_delay_in_minutes>180] <- "High"
 
-
-hist(cleanData$Flight_time_in_minutes)
 Flight.time <- replicate(length(cleanData$Flight_time_in_minutes),"Average")
 Flight.time[cleanData$Flight_time_in_minutes<=100] <- "low"
 Flight.time[cleanData$Flight_time_in_minutes >300] <-"High"
 
-hist(cleanData$Flight_distance)
 FlightDist<- replicate(length(cleanData$Flight_distance),"Average")
 FlightDist[cleanData$Flight_distance <=1200] <- "less"
 FlightDist[cleanData$Flight_distance >1200] <- "More"
 
-hist(cleanData$Age)
 q <- quantile(cleanData$Age, c(0.4, 0.6))
 Age <- replicate(length(cleanData$Age), "Average")
 Age[cleanData$Age <= q[1]] <- "Low"
@@ -59,9 +45,5 @@ df
 rules<-apriori(df,parameter = list(support=0.1, confidence=0.5),appearance = list(default="lhs", rhs=("satisfied=no")))
 summary(rules)
 inspect(rules)
-hist(quality(rules)$lift)
-plot(rules,jitter = 0)
 goodrules<- rules[quality(rules)$lift > 2.0] #Airline status = blue and type of travel= personal travel are the ones having hgihest confidence
 inspect(goodrules)
-plot(goodrules)
-itemFrequencyPlot(df,support=0.1)
