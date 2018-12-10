b78266732336d93225893c51c7efe324f869cd63
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:41:03 -0500

    Saving Apriori_selected.R

--- Apriori_selected.R
+++ Apriori_selected.R
@@ -3,7 +3,6 @@
 
 library(arules)
 library(arulesViz)
-library(dplyr)
 cleanData$Satisfaction <- as.numeric(as.character(cleanData$Satisfaction))
 
 satisfied <- ifelse(cleanData$Satisfaction < 3.5,"no","yes")
